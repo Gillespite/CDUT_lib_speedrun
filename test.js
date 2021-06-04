b8a589e2b5073348f9fffb1a4601c3993e9b861c
Gillespite <42411503+Gillespite@users.noreply.github.com>
2021-06-04 20:53:58 +0800

    Update test.js

--- test.js
+++ test.js
@@ -36,9 +36,8 @@ var run = function () {
 run();
 
 var taskitem = function () {
-  lijipeng();
-  guotianyin();
-  yeyunxi();
+  people1();
+  people2();
 }
 
 var people1 = function () {
@@ -65,24 +64,8 @@ var people1 = function () {
 }
 
 var people2 = function () {
-  fetch("http://mlib.cdut.cn/yy/WxYy2.aspx?openId=oozmJs1RG0Hgt2cDIV9kdTyRRA7g&timestamp=1622470160", {
-    "headers": {
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-language": "zh-CN,zh;q=0.9",
-      "cache-control": "max-age=0",
-      "content-type": "application/x-www-form-urlencoded",
-      "upgrade-insecure-requests": "1",
-      "cookie": "ASP.NET_SessionId=lx4avzrrm3luiyowvujhotfy"
-    },
-    "referrer": "http://mlib.cdut.cn/yy/WxYy2.aspx?openId=oozmJs1RG0Hgt2cDIV9kdTyRRA7g&timestamp=1622470160",
-    "referrerPolicy": "strict-origin-when-cross-origin",
-    "body": "__VIEWSTATE=8UAqr4imZ%2Fwe9aStux5ZR8FxSp3RYKkB4RxMPS%2BcuKV3DaLxe%2B3mEQIiy%2F6FR%2FyMj%2BMN4aBuGt6mfL6JHBnTVaPzeLsNHHPXNe5GKa%2BD2c%2Fk1iyFSn1VkzwLRfYP4Qrw5D3QHl7sItnylWrcD4390GRYFLwrZKFDeHL5K6MuKjRixuWQgzUwCKWMwYntRCS0WiamxcwEoAZWkqCS%2BShlHbnRt1waHRkKaXtjBAFvkTlTBz%2Fh4V0rXPQDTJ%2FeS15MnsAHdPm63trVziwlX%2BeY9UT0G80pcM%2BsG1dOgYdWDjQzi704alq57zR8BoyGBrPl%2BLG07G8%2FvVPM92tQH3jjh%2FlnjJXjx%2FkMcABmLDiO9Oxc6wwlcb0jOWcMSqzQfHMnm%2FPp3vpH6p7tg8%2Fw2ouiP0QYIRIHGX0E1O9gR9qhAmRsfeD0kuhDMIxNchQQdzNrTLmAq6cyk0dLxdQ4Zb4hQ7EapgAXYiY%2FiE%2F8ZIjSyNk%3D&__VIEWSTATEGENERATOR=29D80D0A&__EVENTVALIDATION=nNEPFoeWRPLG%2FRArHVRT3PfjIFeAv5BfklEb7F%2FiTLJluFj6Khwpoqv0S%2FJdzFoqw1IjSTDfLvqPR7KEM9VJfx0Zlq17oQK2GgjSxj%2ByRZkGAPWwsMoSx%2BpnMbycnbErrE1PLKGN0qT3LeyGHwTZFJ4xvUnWdRhiRWhs8XV0%2BkcIccXte4XQuZZ43BwSgnJ16ouIL%2FT1WzdB%2BKgilOLgFJYabexARBeua00NbpKpa23wwVwde7gNiGC6KYdeQTvv&hiddenOpenId=oozmJs1RG0Hgt2cDIV9kdTyRRA7g&hiddenTimestamp=" + now + "&card_id=&card_password=&TextBox1=&Reservation=%E9%A2%84%E7%BA%A6%E5%85%A5%E9%A6%86",
-    "method": "POST",
-    "mode": "cors"
-  }).then((response) => {
-    console.log("people2:" + now)
-    //console.log(response)
-  })
+  //可以按该格式添加多个人
+  //添加后加到taskitem里
 }
 
 }
